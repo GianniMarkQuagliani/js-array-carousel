2253178fe38b0a64c49b82ce5a9f0c81e556adc6
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-13 15:13:48 +0200

    [add] js

---
+++ script.js
@@ -0,0 +1,33 @@
+//elements
+const itemsWrapper = document.querySelector('.items-wrapper');
+const circlesWrapper = document.querySelector('.circles');
+const btnNext = document.querySelector('.right');
+const btnPrev = document.querySelector('.left');
+
+//nascondo di defoult il bottone prev
+btnPrev.classList.add('hide');
+
+const images = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp',
+]
+
+//resetto lo slider
+itemsWrapper.innerHTML = '';
+circlesWrapper.innerHTML = '';
+
+//con un ciclo stampo tutte le immagini dentro a items-wrapper
+for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+    console.log(image);
+    itemsWrapper.innerHTML += `<img src="${image}" class="item hide"></img>`;
+}
+
+//prendo tutti gli elementi ocn la classe item e li salvo in un array
+const itemsCollection = document.getElementsByClassName('item');
+
+//al primo elemento tolgo la classe hide
+itemsCollection[counterImg].classList.remove('hide');
